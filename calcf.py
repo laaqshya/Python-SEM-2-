a53e88b85a094e6ff6772e8535f59c1de20a2fb1
Laaqshya <laaqshya@gmail.com>
2026-04-21 15:42:53 +0530

    Create calcf.py

---
+++ calcf.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+a = 10
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", sub(a, b))
+print("Multiplication:", mul(a, b))
+print("Division:", div(a, b))
